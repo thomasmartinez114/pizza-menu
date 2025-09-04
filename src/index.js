70065ea93a406f16093fef731954b2b72b61d9f5
Thomas Martinez <devtommie114@gmail.com>
2025-09-04 00:59:46 -0400

    set up pizza to be props

--- src/index.js
+++ src/index.js
@@ -72,10 +72,33 @@ function Menu() {
   return (
     <main className='menu'>
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name='Pizza Spinaci'
+        ingredients='Tomato, mozarella, spinach, and ricotta cheese'
+        photoName='pizzas/spinaci.jpg'
+        price={10}
+      />
+
+      <Pizza
+        name='Pizza Funghi'
+        ingredients='Tomato, mushrooms'
+        price={13}
+        photoName='pizzas/funghi.jpg'
+      />
     </main>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <div className='pizza'>
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 3}</span>
+      </div>
+    </div>
   );
 }
 
@@ -93,16 +116,6 @@ function Footer() {
     <footer className='footer'>
       {new Date().toLocaleTimeString()}. We're currently open
     </footer>
-  );
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src='pizzas/spinaci.jpg' alt='spinaci' />
-      <h3>Pizza Margherita</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
   );
 }
 
